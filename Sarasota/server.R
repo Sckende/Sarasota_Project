25ae778fa85c6867332a92d775e8d6310e9eecf5
Sckende <clrccljuhasz@gmail.com>
2020-07-28 16:33:26 -0400

    Mini avancées

--- Sarasota/server.R
+++ Sarasota/server.R
@@ -2,13 +2,28 @@ library(shiny)
 library(shinydashboard)
 
 motion <- read.csv2("C:/Users/HP_9470m/Documents/Sarasota_Equisense/Sarasota_28-6-2020_16h43_motion.csv", h = T, sep = ",")
+sym <- as.numeric(motion$symmetry[as.numeric(motion$symmetry) > 0])
+sym <- as.vector(na.omit(sym))
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  output$histogram <- renderPlot(
-    hist(as.numeric(motion$symmetry[as.numeric(motion$symmetry) > 0]),
-         breaks = input$bins,
+  output$histogram0 <- renderPlot({
+    # sym_values <- switch(input$sym_value,
+    #                      "Less5" = sym[sym < 5],
+    #                      "From5to7" = sym[sym < 7 & sym >= 5],
+    #                      "More7" = sym[sym >=7],
+    #                      sym)
+    barplot(sym,
+            col = ifelse(sym >= 7, "green",
+                         ifelse(sym < 5, "red", "yellow"))
+      
+    )
+  })
+  output$histogram1 <- renderPlot(
+    j <- hist(sym,
+         col = ifelse(j$breaks >= 7, "green",
+                      ifelse(j$breaks < 5, "red", "yellow")),
+         breaks = 12,
          main = "",
-         xlab = "Symetry per half stride")
-  )
+         xlab = "Symetry per half stride"))
 })
--- Sarasota/ui.R
+++ Sarasota/ui.R
@@ -5,16 +5,20 @@ shinyUI(
   dashboardPage(
     dashboardHeader(title = "Sarasota"),
     dashboardSidebar(
+      box(tags$img(height = 100,
+                   width = 100,
+                   src = "Images/fox.jpg")),
       menuItem("Metrics"),
         menuSubItem("Symetry"),
         menuSubItem("Regularity"),
         menuSubItem("Cadence")),
-    dashboardBody(title = "This is the body",
-                  fluidPage(
-                    box(plotOutput("histogram")),
-                    box(sliderInput("bins", "Number of breaks", 10, 30, 1)),
-                    box(tags$img(height = 100,
-                                 width = 100,
-                                 src = "Images/fox.jpg")),
+    dashboardBody(fluidPage(
+                    box(radioButtons(inputId = "sym_values",
+                                     label = "Symetry values",
+                                     choices = c("Less than 5" = "Less5",
+                              "From 5 to 7" = "From5to7",
+                              "More than 7" = "More7")),
+                        plotOutput("histogram0"),
+                    box(plotOutput("histogram1")),
                     box(tags$a(href = "https://vimeo.com/rstudioinc/review/131218530/212d8a5a7a/#t=1h36m54s", "Shiny for beginners"))
                   ))))
