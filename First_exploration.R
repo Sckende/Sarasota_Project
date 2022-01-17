f1432358059cf6efae95a25fec45a88851496196
Sckende <clrccljuhasz@gmail.com>
2022-01-17 14:59:51 +0400

    # Mise en forme

--- First_exploration.R
+++ First_exploration.R
@@ -1,7 +1,9 @@
 # Indices for each session are available at https://research.equisenseapi.com/
 # Then, file.csv are stored on my computer in the "Documents" folder
 setwd("C:/Users/HP_9470m/Documents/Sarasota_Equisense")
-motion <- read.csv2("Sarasota_28-6-2020_16h43_motion.csv", h = T, sep = ",")
+motion <- read.csv2("Sarasota_28-6-2020_16h43_motion.csv",
+                     h = T,
+                     sep = ",")
 
 #### Here is the test zone ####
 head(motion)
@@ -14,4 +16,5 @@ summary(motion$symmetry)
 motion <- motion[!motion$symmetry == "NaN",]
 table(motion$symmetry, useNA = "always")
 
-plot(as.numeric(motion$symmetry), col = ifelse(as.numeric(motion$symmetry) <= 5, "red", "green"))
+plot(as.numeric(motion$symmetry),
+                col = ifelse(as.numeric(motion$symmetry) <= 5, "red", "green"))
